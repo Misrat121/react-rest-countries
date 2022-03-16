ff0dc13205651bd48b7dd6b960df68cef2b62f7a
Misrat121 <you@example.com>
2022-03-16 23:15:06 +0600

    unique key added

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -12,17 +13,16 @@ const Countries = () => {
 
     
     return (
-        <div>
+        <div >
             <h1>Hello From Countries : {countries.length}</h1>
-            {
-                countries.map(country =>console.log(country))
-            }
-            
-            {
+          <div className='countries-container'>
+          {
                 countries.map(country => <Country
                 country = {country}  
+                key = {country.cca3}
                      ></Country>)
             }
+          </div>
         </div>
     );
 };
